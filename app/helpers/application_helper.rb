9bc973621d5cab5cf44c6f1d0a81a61b523f3648
Naofumi Kagami <naofumi@mac.com>
2026-02-17 10:13:27 +0900

    Add Rouge syntax highlighting for showcase code panels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,30 @@
 module ApplicationHelper
+  def highlight_showcase_code(code, language)
+    formatter.format(lexer(language).lex(code.to_s))
+  end
+
+  def showcase_code_language_from_title(title)
+    normalized = title.to_s.downcase
+    return "jsx" if normalized.include?("react")
+    return "erb" if normalized.include?("erb")
+
+    "plaintext"
+  end
+
+  private
+
+  def formatter
+    @formatter ||= Rouge::Formatters::HTML.new
+  end
+
+  def lexer(language)
+    case language.to_s
+    when "erb"
+      Rouge::Lexers::ERB.new
+    when "jsx", "javascript", "js"
+      Rouge::Lexers::Javascript.new
+    else
+      Rouge::Lexers::PlainText.new
+    end
+  end
 end
